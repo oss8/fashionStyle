d38b15b81d9d0d7789d42df51cfe48a991d436d3
ling <geling1974@gmail.com>
2017-08-10 14:56:16 +0800

    chang code

--- common/models/f-design.js
+++ common/models/f-design.js
@@ -25,15 +25,18 @@ module.exports = function (Fdesign) {
 
         bsSQL = "select name, concat(publicId,'.png') as file from cd_publicImage";
         var _publicImage = {};
-        pv.push(ExecuteSyncSQLResult(bsSQL, _publicImage));        
+        pv.push(ExecuteSyncSQLResult(bsSQL, _publicImage));
 
 
         bsSQL = "select name, value from cd_colorVersion where baseId = '" + baseInfo.Typeversion + "'";
         var _colorVersion = {};
-        pv.push(ExecuteSyncSQLResult(bsSQL, _colorVersion));  
+        pv.push(ExecuteSyncSQLResult(bsSQL, _colorVersion));
 
         Promise.all(pv).then(function () {
-
+            if (_baseVersion.Result.length == 0) {
+                cb(Error('基础版型未找到！'), { status: 0, "result": "" });
+                return;
+            }
             var result = {};
 
             result.Gender = baseInfo.Gender;
@@ -41,7 +44,7 @@ module.exports = function (Fdesign) {
             result.data = {};
 
 
-            var _A = {  };
+            var _A = {};
             _A.neck = {};
             _A.neck.name = _baseVersion.Result[0].neckName;
             _A.neck.fileList = _.filter(_A_detailVersion.Result, function (fitem) { return fitem.positionType == '01' });
@@ -54,11 +57,17 @@ module.exports = function (Fdesign) {
             _A.ornament.name = _baseVersion.Result[0].ornament;
             _A.ornament.fileList = _.filter(_A_detailVersion.Result, function (fitem) { return fitem.positionType == '03' });
 
-            _A.color = _colorVersion.Result;
-            _A.pattern = _publicImage.Result;
+            _A.color = {};
+            _A.color.name = '颜色';
+            _A.color.fileList = _colorVersion.Result;
+
+
+            _A.pattern = {};
+            _A.pattern.name = '贴图';
+            _A.pattern.fileList = _publicImage.Result;
             result.data.A = _A;
 
-            var _B = {  };
+            var _B = {};
             _B.neck = {};
             _B.neck.name = _baseVersion.Result[0].neckName;
             _B.neck.fileList = _.filter(_B_detailVersion.Result, function (fitem) { return fitem.positionType == '01' });
@@ -71,10 +80,14 @@ module.exports = function (Fdesign) {
             _B.ornament.name = _baseVersion.Result[0].ornament;
             _B.ornament.fileList = _.filter(_B_detailVersion.Result, function (fitem) { return fitem.positionType == '03' });
 
-            _B.color = _colorVersion.Result;
-            _B.pattern = _publicImage.Result;
+            _B.color = {};
+            _B.color.name = '颜色';
+            _B.color.fileList = _colorVersion.Result;
+            _B.pattern = {};
+            _B.pattern.name = '贴图';
+            _B.pattern.fileList = _publicImage.Result;
 
-            result.data.B = _B;      
+            result.data.B = _B;
 
 
             cb(null, { status: 1, "result": result });
@@ -88,13 +101,13 @@ module.exports = function (Fdesign) {
         {
             http: { verb: 'post' },
             description: '获取指定版型的图片信息',
-            accepts: { arg: 'baseInfo', type: 'object', description: '{"Gender":"A","Typeversion":"00"}' },
+            accepts: { arg: 'baseInfo', type: 'object', description: '{"Gender":"A","Typeversion":"02"}' },
             returns: { arg: 'baseInfo', type: 'object', root: true }
         }
     );
 
 
-    Fdesign.GetBaseInfo = function ( cb) {
+    Fdesign.GetBaseInfo = function (cb) {
         EWTRACE("GetBaseInfo Begin");
 
         var bsSQL = "select baseId,baseName,Gender from cd_baseVersion";
@@ -103,10 +116,10 @@ module.exports = function (Fdesign) {
 
             var _result = {};
 
-            _result.M = _.filter( result, function(fitem){
+            _result.M = _.filter(result, function (fitem) {
                 return fitem.Gender == "M" || fitem.Gender == "A";
             })
-            _result.W = _.filter( result, function(fitem){
+            _result.W = _.filter(result, function (fitem) {
                 return fitem.Gender == "W" || fitem.Gender == "A";
             })
 
@@ -123,5 +136,5 @@ module.exports = function (Fdesign) {
             description: '获取版型的基础信息',
             returns: { arg: 'baseInfo', type: 'object', root: true }
         }
-    );    
+    );
 };
